cb61adf66f33c8a16b9cedbcf02ce57a4c526519
RahulNathani49 <nathanirahul49@gmail.com>
2020-05-02 12:13:12 +0530

    changes

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var app = express();
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact', { qs: req.query });
+
+});
+app.post('/contact', urlencodedParser, function(req, res) {
+    res.render('contactdis', { qs: req.body });
+});
+
+app.get('/profile/:id', function(req, res) {
+    var data = { age: 29, job: 'Developer' };
+    res.render('index', { person: req.params.id, data: data });
+});
+
+app.listen(3000);
+console.log("APP STARTED");
